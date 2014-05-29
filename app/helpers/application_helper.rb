ba240626de2cff8170c749c43ef000c690926013
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-05-29 16:43:29 -0700

    Adding and styling hero image.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def delete_confirm(name)
     { method: :delete, data: { confirm: "Are you sure you want to delete #{name}?" } }
   end
+
+  def hero_title
+    content_tag :div, class: 'hero-title' do
+      content_for?(:hero_title) ? yield(:hero_title) : content_tag(:h1) { 'Descendants' }
+    end
+  end
 end
