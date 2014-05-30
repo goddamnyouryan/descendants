8fd9774aaded04420418e48b4f97911305a484be
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-05-29 17:27:52 -0700

    Cleaning up styles a bit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,16 @@ module ApplicationHelper
 
   def hero_title
     content_tag :div, class: 'hero-title' do
-      content_for?(:hero_title) ? yield(:hero_title) : content_tag(:h1) { 'Descendants' }
+      content_for(:hero_title) || content_tag(:h1) { 'Descendants' }
+    end
+  end
+
+  def hero_image
+    if content_for?(:hero)
+      image_tag(content_for(:hero))
+    else
+      random = (1..4).to_a.sample
+      image_tag("/bg-filler-#{random}.jpg")
     end
   end
 end
