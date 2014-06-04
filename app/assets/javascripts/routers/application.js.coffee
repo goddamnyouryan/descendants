520711a1f7b21f56285f7d0612fcfc48145203e2
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-06-03 21:15:16 -0700

    Moving directors section to above header.

--- app/assets/javascripts/routers/application.js.coffee
+++ app/assets/javascripts/routers/application.js.coffee
@@ -2,10 +2,15 @@ class Descendants.Routers.Application extends Backbone.Router
 
   routes: ->
     '': 'home'
+    'directors/:slug': 'directorsShow'
     'admin/directors/:director/videos/:video/edit': 'videoForm'
 
   home: ->
     new Descendants.Views.Contact el: 'form'
+    new Descendants.Views.Directors el: 'body'
+
+  directorsShow: (slug) ->
+    new Descendants.Views.Directors el: 'body'
 
   videoForm: (director, video) ->
     new Descendants.Views.VideoForm el: '#s3_uploader'
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,9 +1,9 @@
 class DirectorsController < ApplicationController
   before_action :set_director, only: :show
+  before_action :set_directors
   before_action :render_preview?, only: :index
 
   def index
-    @directors = Director.includes(:videos).all.map {|director| director.videos.sample }
     @recent = Video.limit(4).order('created_at desc')
   end
 
@@ -12,6 +12,10 @@ class DirectorsController < ApplicationController
   end
 
   private
+
+  def set_directors
+    @directors = Director.includes(:videos).all.map {|director| director.videos.sample }
+  end
 
   def render_preview?
     render layout: 'preview', action: :preview if ENV['PREVIEW_PAGE']
