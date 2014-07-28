aec37a623b3470921e83c23fab6663e699c7c9dd
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-07-28 15:56:29 -0300

    Making site live.

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,7 +1,6 @@
 class DirectorsController < ApplicationController
   before_action :set_director, only: :show
   before_action :set_directors, only: :index
-  before_action :render_preview?, only: :index
 
   def index
     @joinery_featured = Director.joinery.first.featured
@@ -9,11 +8,5 @@ class DirectorsController < ApplicationController
 
   def show
     @videos = @director.videos
-  end
-
-  private
-
-  def render_preview?
-    render layout: 'preview', action: :preview if ENV['PREVIEW_PAGE'] && !params[:beta]
   end
 end
