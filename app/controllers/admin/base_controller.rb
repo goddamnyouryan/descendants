795c93a6edbe46d80c74907b8f84e6a04e4de770
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-08-15 11:45:51 -0700

    Adding admin password.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,6 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
-  # before_filter :password_protect
+  before_filter :password_protect
 
   def index
     @directors = Director.all
@@ -9,8 +9,9 @@ class Admin::BaseController < ApplicationController
   private
 
   def password_protect
+    return unless Rails.env.production?
     authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == 'admin' && password == 'password'
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
     end
   end
 end
